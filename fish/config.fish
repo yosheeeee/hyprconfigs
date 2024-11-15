bcf9aecf9f0ed3f0344259cc7aa196eaf635e9c0
Antipin Egor <evantipin@edu.hse.ru>
2024-11-15 16:23:10 +0500

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,1 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
 set -gx EDITOR nvim
-set -U fish_user_paths /home/yoshee/.nvm/versions/node/v20.17.0/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:/usr/local/bin:/usr/bin:/var/lib/snapd/snap/bin:$fish_user_paths
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
