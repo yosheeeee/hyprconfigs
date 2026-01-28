643d8218cc31f49d1c3c8b759cdf9990e76ed0cc
e.antipin <113037627+yosheeeee@users.noreply.github.com>
2026-01-28 09:30:55 +0500

    update configs

---
+++ nvim/lua/plugins/ayu_theme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "Shatur/neovim-ayu",
+  },
+  {
+    "Yazeed1s/oh-lucy.nvim",
+  },
+  {
+    "mellow-theme/mellow.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ayu",
+    },
+  },
+}
---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,34 @@
+return {
+  "lualine.nvim",
+  opts = function()
+    return {
+      options = {
+        theme = "auto",
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_b = { "filename", "branch" },
+        lualine_c = {
+          "%=", -- Это выравнивает модули, оставляя центр пустым (и теперь прозрачным)
+        },
+        lualine_x = {},
+        lualine_y = { "filetype", "progress" },
+        lualine_z = {
+          { "location", separator = { right = "" }, left_padding = 2 },
+        },
+      },
+      inactive_sections = {
+        lualine_a = { "filename" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "location" },
+      },
+      tabline = {},
+      extensions = {},
+    }
+  end,
+}
